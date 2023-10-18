776c9b24069749c312cec7319e9316ea54666166
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-10-18 19:36:45 +0000

    change in counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,6 +8,8 @@ const Counter = () =>{
        <div>
           <h3 id="calc">Count : {count}</h3>
           <button id="incr-cnt" onClick={()=>setCount(count+1)}>0</button>
+          <h3>Expensive Calculation</h3>
+          <p>1000000000</p>
        </div>
     )
 }
